16610e820a4679c9e48a510a83ff5a1b1fafdbeb
Niklas Korz <niklas@niklaskorz.de>
2021-06-11 12:38:01 +0200

    Add another test for importing modules at the top of the hierarchy

--- tests/test.nim
+++ tests/test.nim
@@ -37,6 +37,10 @@ suite "nimscripter":
     buff = ""
     1000.addToBuffer(buff)
     check 10000 == intr.get.invoke("doThingExported", buff, int)
+  test "Import flat standard modules":
+    let script = "import strutils" # stdlib/pure/strutils.nim
+    let intr = loadScript(script, false)
+    check intr.isSome
   test "Import deep standard modules":
     let script = "import sequtils" # stdlib/pure/collections/sequtils.nim
     let intr = loadScript(script, false)
